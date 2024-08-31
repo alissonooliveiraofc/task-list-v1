f1ac192c192c8bfb0aaab13a195d54c2f207b213
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-08-31 18:17:06 -0300

    Req 8, 9 - Done

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ button.addEventListener('click', () => {
   list.appendChild(task);
   input.value = '';
 });
+
+// Adiciona cor cinza ao clicar na tarefa
+list.addEventListener('click', (event) => {
+  const selected = document.querySelector('.selected');
+  if (selected !== null) {
+    selected.classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+});
+
+// Adiciona riscado ao clicar duas vezes na tarefa
+list.addEventListener('dblclick', (event) => {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+});
