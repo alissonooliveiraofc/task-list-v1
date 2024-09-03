b7a09877fd20938cff1dbad1b53dfef35272f19b
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-09-03 00:15:41 -0300

    Adiciona Tarefas ao Storage

--- script.js
+++ script.js
@@ -7,27 +7,17 @@ const clearDonedTasks = document.getElementById('remover-finalizados');
 // Adiciona tarefa na lista
 button.addEventListener('click', () => {
   const task = document.createElement('li');
-  task.innerText = input.value;
+  const checkbox = document.createElement('input');
+  const label = document.createElement('label');
+
+  checkbox.className = 'task';
+  checkbox.type = 'checkbox';
+  label.innerText = input.value;
+  task.appendChild(checkbox);
+  task.appendChild(label);
+
   list.appendChild(task);
   input.value = '';
-});
-
-// Adiciona cor cinza ao clicar na tarefa
-list.addEventListener('click', (event) => {
-  const selected = document.querySelector('.selected');
-  if (selected !== null) {
-    selected.classList.remove('selected');
-  }
-  event.target.classList.add('selected');
-});
-
-// Adiciona riscado ao clicar duas vezes na tarefa
-list.addEventListener('dblclick', (event) => {
-  if (event.target.classList.contains('completed')) {
-    event.target.classList.remove('completed');
-  } else {
-    event.target.classList.add('completed');
-  }
 });
 
 // Remove todas as tarefas
@@ -42,3 +32,72 @@ clearDonedTasks.addEventListener('click', () => {
     task.remove();
   });
 });
+
+// Adiciona Tasks ao LocalStorage
+const saveTasks = () => {
+  const tasks = document.querySelectorAll('li');
+  const tasksArray = [];
+
+  tasks.forEach((task) => {
+    const obj = {
+      text: task.firstElementChild.nextElementSibling.innerText,
+      completed: task.classList.contains('completed'),
+    };
+
+    tasksArray.push(obj);
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasksArray));
+};
+
+button.addEventListener('click', saveTasks);
+
+// Adiciona riscado ao marcar checkbox
+list.addEventListener('click', (event) => {
+  const task = event.target;
+  if (task.className === 'task') {
+    if (task.checked) {
+      task.parentElement.classList.add('completed');
+      saveTasks();
+    } else {
+      task.parentElement.classList.remove('completed');
+    }
+  }
+});
+
+// Recupera Tasks do LocalStorage
+const loadingTasks = () => {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+  tasks.forEach((task) => {
+    const taskElement = document.createElement('li');
+    const checkbox = document.createElement('input');
+    const label = document.createElement('label');
+
+    checkbox.className = 'task';
+    checkbox.type = 'checkbox';
+    label.innerText = task.text;
+    taskElement.appendChild(checkbox);
+    taskElement.appendChild(label);
+
+    list.appendChild(taskElement);
+  });
+};
+
+const checkFromStorage = () => {
+  loadingTasks();
+  if (localStorage.getItem('tasks')) {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.forEach((task, index) => {
+      const taskElement = document.querySelectorAll('li')[index];
+      if (task.completed === true) {
+        taskElement.classList.add('completed');
+        taskElement.firstElementChild.checked = true;
+      }
+    });
+  }
+};
+
+window.onload = () => {
+  checkFromStorage();
+};
