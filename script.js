3bc05f565f8dcd10739b26ee1f11b57d2adda031
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-09-03 17:38:39 -0300

    fix: Salva tarefas no Storage a cada modificação

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const list = document.getElementById('lista-tarefas');
 const clearButton = document.getElementById('apaga-tudo');
 const clearDonedTasks = document.getElementById('remover-finalizados');
 const audio = document.getElementById('task-doned');
+// const concluidas = document.getElementById('concluidas');
 
 // Adiciona tarefa na lista
 button.addEventListener('click', () => {
@@ -104,6 +105,30 @@ const checkFromStorage = () => {
   }
 };
 
+const createDescription = () => {
+  const tittle = document.createElement('h2');
+  const doned = document.createElement('h2');
+
+  tittle.innerText = 'A fazer';
+  doned.innerText = 'Concluídas';
+
+  button.addEventListener('click', () => {
+    if (list.children.length !== 0) {
+      list.insertBefore(tittle, list.firstChild);
+    } else if (list.children === tittle) {
+      list.removeChild(tittle);
+    }
+  });
+};
+
+// Adiciona tarefa ao pressionar Enter
+input.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    button.click();
+  }
+});
+
 window.onload = () => {
   checkFromStorage();
+  createDescription();
 };
